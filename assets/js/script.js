70ea9fb782b65a90079c09e26b5b2ebf1d0a4bc4
theoL <theo1007@outlook.fr>
2019-01-21 14:35:57 +0100

    Ajout du dossier assets

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+function getSee() {
+  var firstNumber = document.getElementById('firstNumber').value;
+  var secondNumber = document.getElementById('secondNumber').value;
+
+  if (isNaN(firstNumber)==false && isNaN(secondNumber)==false){
+    var result = firstNumber % secondNumber;
+    alert(result);
+  }else{
+    alert("Vous devez rentrez une chaine de nombre");
+  }
+}
